e106a9f269120a96350b1513dba0cc2ddc614c71
blat-blatnik <blatblatnik@protonmail.com>
2022-06-26 00:24:17 +0200

    Natvis.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@ int main(void)
 {
 	InitWindow(1280, 720, "Who Stole The Sun");
 
+	Vector2 x = { 1, 2 };
+	Vector3 y = { 1, 2, 3 };
+	Vector4 z = { 1, 2, 3, 4 };
+	Matrix w = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
