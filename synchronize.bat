544450ac36a7270f3cfd0ea73fb643b8ff3c4d57
blat-blatnik <blatblatnik@protonmail.com>
2022-06-25 23:02:48 +0200

    Synchronize script for git noobs.

--- synchronize.bat
+++ synchronize.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-echo Pulling changes from web repository
+echo Pulling changes from web repository.
 git pull
 
-echo Checking for local changes
+echo Checking for local changes.
 git add .
 git update-index --refresh
 set /a changes=0
 git diff-index --quiet HEAD -- || set /a changes=1
 
 if %changes% == 1 (
-	echo Found local changes
+	echo Found local changes.
 	set "message=[no commit message]"
 	set /p "message=Enter a commit message: "
 	git commit -a -m "!message!"
