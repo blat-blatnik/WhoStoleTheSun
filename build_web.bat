94870c39f8f635c28521cb96e8c5adb77480cebd
blat-blatnik <blatblatnik@protonmail.com>
2022-06-25 21:30:28 +0200

    Hello window.

---
+++ build_web.bat
@@ -0,0 +1,31 @@
+rem # Compile for the web using Emscripten.
+rem # Assumes that emsdk is on you PATH.
+rem # https://github.com/raysan5/raylib/wiki/Working-for-Web-(HTML5)
+
+@echo off
+setlocal EnableDelayedExpansion
+
+rem # Setup emsdk
+rem call emsdk update
+rem call emsdk install latest
+call emsdk activate latest
+
+rem # Collect all .c files for compilation into the 'input' variable.
+for /f %%a in ('forfiles /s /m *.c /c "cmd /c echo @relpath"') do set input=!input! "%%~a"
+
+rem # Compile and link
+call emcc -o bin/web/index.html -Wall -L./lib -lraylib_web -s USE_GLFW=3 -s TOTAL_MEMORY=16777216 --shell-file webshell.html --preload-file res %input%
+
+rem # Make sure .data is smaller than 32MB
+set /a size=0
+for /f "usebackq" %%A in ('bin/web/index.data') do set /a size=%size%+%%~zA
+if %size% geq 33554432 (
+	echo WARNING!
+	echo .data is larger than 32MB!
+)
+
+rem # Copy the favicon
+rem copy /y "Pictomage.ico" "bin/web/favicon.ico"
+
+pause
+rem # Run with: $ emrun bin/web/index.html
--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include "lib/raylib.h"
 
 int main(void)
 {
+	InitWindow(1280, 720, "Who Stole The Sun");
 
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello, world!", 500, 300, 40, GRAY);
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
