830c2e298e832f296df0e78b6318d8454e334fa7
Olga <vana@acdcfan.com>
2022-06-27 23:54:36 +0200

    Testing

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ void GameInit(void)
 void GameLoopOneIteration(void)
 {
 	// some nice comment
+	// yeeees, very nice :)
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
 	DrawText("Hello, world! :)", 500, 300, 40, GRAY);
