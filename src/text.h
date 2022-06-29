9de06cb8b5a4a66f1f5fe60df7167809bf7754e6
blat-blatnik <blatblatnik@protonmail.com>
2022-06-30 00:27:55 +0200

    Removed C++ testing stuff

--- src/text.h
+++ src/text.h
@@ -12,9 +12,6 @@ void DrawFormat(Font font, float x, float y, float fontSize, Color color, FORMAT
 // Same as DrawFormat but takes an explicit varargs pack.
 void DrawFormatVa(Font font, float x, float y, float fontSize, Color color, FORMAT_STRING format, va_list args);
 
-// REMOVE THIS, it's just for testing if C++ works.
-int TestCpp(void);
-
 #ifdef __cplusplus
 }
 #endif
