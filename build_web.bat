34f8481fde27dc843f4df83561eebcdc4f68b9ce
blat-blatnik <blatblatnik@protonmail.com>
2022-06-29 23:47:10 +0200

    Removed old junk from web build scripts.

--- build_web.bat
+++ build_web.bat
@@ -6,15 +6,15 @@ rem # https://github.com/raysan5/raylib/wiki/Working-for-Web-(HTML5)
 setlocal EnableDelayedExpansion
 
 rem # Setup emsdk
-rem call emsdk update
-rem call emsdk install latest
-call emsdk activate latest
+rem emsdk update
+rem emsdk install latest
+emsdk activate latest
 
 rem # Collect all .c files for compilation into the 'input' variable.
 for /f %%a in ('forfiles /s /m *.c /c "cmd /c echo @relpath"') do set input=!input! "%%~a"
 
 rem # Compile and link
-call emcc -o bin/web/index.html -Os -flto -Wall -L./lib -lraylib_web -s USE_GLFW=3 -s TOTAL_MEMORY=16777216 --shell-file webshell.html --preload-file res %input%
+emcc -o bin/web/index.html -Os -flto -Wall -L./lib -lraylib_web -s USE_GLFW=3 -s TOTAL_MEMORY=16777216 --shell-file webshell.html --preload-file res %input%
 
 rem # Copy the favicon
 rem copy /y "Pictomage.ico" "bin/web/favicon.ico"
